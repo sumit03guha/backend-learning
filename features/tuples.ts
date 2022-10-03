962a7142feb793347e424ea1549fd1e677807897
Sumit Guha <sumit03guha@gmail.com>
2022-10-03 15:58:33 +0530

    learnt about tuples

---
+++ features/tuples.ts
@@ -0,0 +1,9 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+type Drink = [string, boolean, number];
+const pepsi: Drink = ['brown', true, 40];
+const tea: Drink = ['black', false, 10];
