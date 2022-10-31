8230fa83a5f3a2d3c859f1eeb2601c36c102461f
Sumit Guha <sumit03guha@gmail.com>
2022-10-31 15:45:35 +0530

    unfinished maps

---
+++ maps/src/index.ts
@@ -0,0 +1,11 @@
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
+new CustomMap('map');
